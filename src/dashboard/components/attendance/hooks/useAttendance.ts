615d9b47aa0f0c7185b59a6fd569f794b7f86a3b
ALEXURI001 <L20530202@cancun.tecnm.mx>
2024-08-16 21:09:23 -0500

    updates

--- src/dashboard/components/attendance/hooks/useAttendance.ts
+++ src/dashboard/components/attendance/hooks/useAttendance.ts
@@ -1,6 +1,19 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+axios.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
 export const useAttendance = (idClass: number) => {
   const [partials, setPartials] = useState<any[]>([]);
   const [selectedPartial, setSelectedPartial] = useState<any | null>(null);
